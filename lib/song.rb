d22f00b74f6af0ac939580876fcd252d5ee39557
Zachary Durr <zdurr22@gmail.com>
2020-11-02 19:50:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,13 +34,14 @@ class Song
   end
 
   def self.find_or_create_by_name(title)
-    answer = self.find_by_name(title)
-    if answer
-      return answer
-    else
-      self.create_by_name
+      #either return a matching song instance with that name or create a new song with the name and return the song instance
+      result = self.find_by_name(title)
+      if result
+        result
+      else
+        self.create_by_name(title)
+      end
     end
-  end
 
 
 
